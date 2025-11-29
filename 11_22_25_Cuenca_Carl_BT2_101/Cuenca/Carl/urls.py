fbc13e64453bc8b81a86e51b66e026d53e599a89
undeadsage <undeadsage00@gmail.com>
2025-11-29 12:24:06 +0800

    Delete 11_22_25_Cuenca_Carl_BT2_101 directory

--- 11_22_25_Cuenca_Carl_BT2_101/Cuenca/Carl/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('show/', views.show_whole_data),
-]
--- 11_22_25_Cuenca_Carl_BT2_101/Cuenca/Carl/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import childClass
-
-# Create your views here.
-def show_whole_data(request):
-    dataClass = childClass.objects.all()
-
-    return render(request, 'home.html', {'childClass':dataClass})
